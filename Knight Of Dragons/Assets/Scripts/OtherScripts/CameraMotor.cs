e0cc194f6e12bc487f01490188825bc192d60d32
ShockDexterity <canterw8353@my.uwstout.edu>
2021-11-11 10:29:41 -0600

    Adding old art
    Creating new sprites from old art. Basically rebuilding from the foundation.

---
+++ Knight Of Dragons/Assets/Scripts/OtherScripts/CameraMotor.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotor : MonoBehaviour
+{
+    public Transform lookAt;
+    public bool followPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        followPlayer = true;
+        if (lookAt == null)
+        {
+            lookAt = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        }
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if(followPlayer)
+        {
+            float deltaX = lookAt.position.x - this.transform.position.x;
+            float deltaY = lookAt.position.y - this.transform.position.y + 1f;
+            transform.position += new Vector3(deltaX, deltaY, 0);
+        }
+    }
+}
